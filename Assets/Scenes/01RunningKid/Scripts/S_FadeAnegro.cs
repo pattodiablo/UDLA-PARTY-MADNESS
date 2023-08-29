4f968dc41a53a646424cf357caf3904fb30b8615
pattodiablo <pattodiablo@gmail.com>
2023-08-28 19:17:09 -0500

    nuevo push

---
+++ Assets/Scenes/01RunningKid/Scripts/S_FadeAnegro.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_FadeAnegro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+public void decirHola(){
+
+
+}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
