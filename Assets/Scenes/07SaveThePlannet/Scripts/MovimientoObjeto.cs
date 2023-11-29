6f914c058e03d126032e4070502474022326cd6a
pattodiablo <pattodiablo@gmail.com>
2023-11-28 21:27:28 -0500

    new tryout

--- Assets/Scenes/07SaveThePlannet/Scripts/MovimientoObjeto.cs
+++ Assets/Scenes/07SaveThePlannet/Scripts/MovimientoObjeto.cs
@@ -11,18 +11,28 @@ public class MovimientoObjeto : MonoBehaviour
 
     public GameObject objetoRotacion;
     public GameObject destroyEffectPrefab;
+ public GameObject plusTenPrefab;
+    
+
+    private GameObject obJectPlusTen; // Asigna el Animator en el Inspector.
 
     private string[] nombresSprites = { "trash1", "trash2", "trash3", "trash4" };
 
      private AudioSource audioSource;
+
+     private Animator plusTenTextAnimator;
 
     // Busca la cámara con el tag "MainCamera" al iniciar el juego
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         Camera mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-
-
+      
+       obJectPlusTen = GameObject.Find("PlusTenText");
+        if (obJectPlusTen != null)
+        {
+         plusTenTextAnimator = obJectPlusTen.GetComponent<Animator>();
+        }
         if (mainCamera == null)
         {
             Debug.LogError("No se encontró una cámara con el tag 'MainCamera'. Asigna el tag correctamente a la cámara principal.");
@@ -53,16 +63,18 @@ public class MovimientoObjeto : MonoBehaviour
             if (touch.phase == TouchPhase.Began)
             {
             
-                  ReproducirSonidoClick();
+               
                 // Convierte la posición del toque a un rayo en el mundo
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
-
+ReproducirSonidoClick();
+                    plusTenTextAnimator.Play("PlusTenText");
                 // Comprueba si el rayo intersecta con el objeto
                 if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == this.gameObject)
-                {
+                {  
+                    
                     Instantiate(destroyEffectPrefab, transform.position, Quaternion.identity).transform.localScale = new Vector3(2f, 2f, 2f);
-
+                     Instantiate(plusTenPrefab, transform.position, Quaternion.identity).transform.localScale = new Vector3(2f, 2f, 2f);
                     Destroy(gameObject);
                 }
             }
