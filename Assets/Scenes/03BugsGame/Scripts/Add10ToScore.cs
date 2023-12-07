259c81ae7b9858948bb4b71a8fec0e0fd5f3371c
pattodiablo <pattodiablo@gmail.com>
2023-12-07 11:25:26 -0500

    viernes 7 dic

---
+++ Assets/Scenes/03BugsGame/Scripts/Add10ToScore.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+
+public class Add10ToScore : MonoBehaviour
+{
+
+private object LocalScore;
+     public void add10(string s)
+    {
+        Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
+        if (Variables.Application.IsDefined("LocalScore"))
+        {
+
+            LocalScore = Variables.Application.Get("LocalScore");
+       if (LocalScore != null)
+        {
+            if (int.TryParse(LocalScore.ToString(), out int scoreActual))
+            {
+                // Suma 10 puntos al score actual
+                scoreActual += 10;
+
+                // Guarda el nuevo valor de score donde sea necesario (por ejemplo, PlayerPrefs)
+                Variables.Application.Set("LocalScore", scoreActual);
+
+                // Puedes imprimir el valor actualizado si lo deseas
+                Debug.Log("LocalScore actualizado: " + scoreActual);
+            }
+            else
+            {
+                Debug.LogError("No se pudo convertir el objeto a un valor entero.");
+            }
+        }
+           
+
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
